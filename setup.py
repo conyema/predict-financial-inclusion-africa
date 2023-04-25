4c32e801ae62f3af869a4f177506513407944030
chinedum onyema <onyemachinedum@gmail.com>
2023-04-25 09:24:17 +0100

    chore(dev-setup): setup development environment for the project
    - Create a virtual development environment
    - Define the list of requirements file
    - Define setup properties to build the project
    - Create a project source

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Import necessary setup packages
+from setuptools import find_packages, setup
+from typing import List
+
+# "-e." triggers setup.py from the requirements.txt
+setup_trigger = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    Returns a list of required libraries/packages
+    '''
+    requirements = []
+
+    with open(file_path) as file_obj:
+        # Save each line in the requirements list
+        requirements = file_obj.readlines()
+        # Use list comprehension to remove '/n' character
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if setup_trigger in requirements:
+            requirements.remove(setup_trigger)
+
+
+    return requirements
+
+setup(
+    name='FIAfrica',
+    version='0.0.1',
+    author='Conyema',
+    author_email='onyemachinedum@gmail.com',
+    packages=find_packages(),
+    install_requrires=get_requirements('requirements.txt')
+)
